7d11809bc2684cd0ed18447e8df560d3ddcb05d9
Alboran <97444137+ImAlboran@users.noreply.github.com>
2023-08-21 16:52:37 +0200

    fix : Exception : division par 0 impossible

--- calculatrice.js
+++ calculatrice.js
@@ -13,6 +13,9 @@ function soustraction(x, y){
 }
 
 function division(x, y){
+    if (y == 0) {
+        throw new Error("Impossible de diviser par 0");
+    }
     return x/y;
 }
 
@@ -39,14 +42,18 @@ switch (choix) {
     case 1: //Addition
         alert("Voici le resultat : " + addition(nombreX(), nombreY()))
         break;
-    case 2: //Addition
+    case 2: //Multpiplication
         alert("Voici le resultat : " + multiplication(nombreX(), nombreY()));
         break;
-    case 3: //Addition
+    case 3: //Soustraction
         alert("Voici le resultat : " + soustraction(nombreX(), nombreY()));
         break;
-    case 4: //Addition
-        alert("Voici le resultat : " + division(nombreX(), nombreY()));
+    case 4: //Division
+        try{
+            alert("Voici le resultat : " + division(nombreX(), nombreY()));
+        } catch(e){
+            alert(e);
+        }
         break; 
     default:
         alert("Rien");
